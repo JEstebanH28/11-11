7b2fdcb6246d87436e819443094f64079f3fc155
Augusto Pavani <pavani.augusto@gmail.com>
2024-02-16 09:29:04 -0500

    arreglo boton sugerencias

--- JS/preferencia.js
+++ JS/preferencia.js
@@ -1,6 +1,6 @@
-function agregarPreferencia() {
-    // Cambiar el texto del botón a "Añadido"
-    document.getElementById('texto-boton').innerText = 'Añadido';
-    // Deshabilitar el botón después de hacer clic para evitar múltiples clics
-    document.getElementById('boton-preferencia').disabled = true;
-  }
+function agregarPreferencia(boton) {
+  // Cambiar el texto del botón a "Añadido"
+  boton.innerText = 'Añadido';
+  // Deshabilitar el botón después de hacer clic para evitar múltiples clics
+  boton.disabled = true;
+}
